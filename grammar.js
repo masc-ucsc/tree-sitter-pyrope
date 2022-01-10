4e208ce1fc8bc7f7d61e96742cd8a6c73e8bae57
Jing-Hsiang Huang <jhuan278@ucsc.edu>
2022-01-10 14:02:35 -0800

    add auto-semicolon

--- grammar.js
+++ grammar.js
@@ -22,7 +22,7 @@ function listseq1(rule) {
 module.exports = grammar({
   name: 'pyrope'
 
-  ,externals: $ => []
+  ,externals: $ => [ $._automatic_semicolon ]
   ,conflicts: $ => [ [$.assignment] ]
   ,extras:    $ => [ $._space, $._comment ]
   ,word:      $ => $.identifier
@@ -81,73 +81,86 @@ module.exports = grammar({
       ,choice(
         // Synthesizable
         $.scope_statement
-        ,$.assignment_or_declaration
+        ,$.assignment_or_declaration_statement
+        ,$.function_call_statement
         ,$.control_statement
         ,$.if_statement
         ,$.for_statement
         ,$.while_statement
         ,$.match_statement
+        ,$.enum_declaration
         ,$.type_declaration
         ,$.type_extension
-        ,$.expression
+        ,$.expression_statement
         // Verification Only
         ,$.test_statement
-        ,$.cover_statement
-        ,$.covercase_statement
-        ,$.assert_statement
-        ,$.verify_statement
-        ,$.wait_statement
-        ,$.step_statement
-        ,$.puts_statement
-        ,$.print_statement
-        ,$.lec_statement
-        ,$.poke_statement
-        ,$.peek_statement
+        ,$.restrict_statement
       )
     ))
     ,scope_statement: $ => prec.left('statement', seq(
       '{'
-      ,repseq($.statement, repeat(';'))
+      ,repseq($.statement)
       ,'}'
       ,repseq(
         '#>'
         ,'{'
-        ,repseq($.statement, repeat(';'))
+        ,repseq($.statement)
         ,'}'
       )
     ))
-    ,assert_statement: $ => prec.right(seq(
-      'assert'
-      ,$.expression
-      ,optseq(',', $.expression)
+    ,assignment_or_declaration_statement: $ => prec.right(seq(
+      $.assignment_or_declaration
+      ,$._semicolon
     ))
-    ,cover_statement: $ => prec.right(seq('cover', $.expression_list))
-    ,covercase_statement: $ => prec.right(seq('covercase', $.expression_list))
-    ,verify_statement: $ => prec.right(seq(
-      'verify'
-      ,$.expression
-      ,optseq(',', $.expression)
+    ,function_call_statement: $ => prec.right(seq(
+      $.simple_function_call
+      ,$._semicolon
     ))
     ,control_statement: $ => prec.right(seq(
-      choice(
+      field('type', choice(
         // TODO: Decide which keywords to use
         'ret', 'return', 'cont', 'continue', 'brk', 'break', 'last'
-      )
-      ,optional($.expression)
+      ))
+      ,field('argument', optional($._expression))
+      ,$._semicolon
+    ))
+    ,enum_declaration: $ => prec.left(seq(
+      'enum'
+      ,field('name', $.identifier)
+      ,field('type', optional($.type_cast))
+      ,field('definition', optseq('=', $._expression))
+      ,$._semicolon
     ))
     ,type_declaration: $ => prec.left(seq(
-      'type', $.identifier, optseq('=', $.expression)
+      'type'
+      ,field('name', $.identifier)
+      ,field('definition', optseq('=', choice(
+        $._expression
+        ,$.primitive_type
+        ,$.array_type
+        ,$.function_type
+      )))
+      ,$._semicolon
     ))
     ,type_extension: $ => prec.left(seq(
-      'type', $.identifier, 'extends', $.expression, 'with', '(', $.tuple_list, ')'
+      'type'
+      ,field('name', $.identifier)
+      ,'extends'
+      ,field('parent', $._expression)
+      ,'with'
+      ,'('
+      ,$.tuple_list
+      ,')'
+      ,$._semicolon
     ))
-    ,if_statement: $ => prec.left(seq(
-      'if'
-      ,field('condition', $.expression)
+    ,if_statement: $ => prec.left('statement', seq(
+      optional('unique')
+      ,'if'
+      ,field('condition', $._expression)
       ,field('code', $.scope_statement)
       ,field('elif', repseq(
         'elif'
-        ,field('condition', $.expression)
+        ,field('condition', $._expression)
         ,field('code', $.scope_statement)
       ))
       ,field('else', optseq(
@@ -157,6 +170,7 @@ module.exports = grammar({
     ))
     ,for_statement: $ => seq(
       'for'
+      ,field('mutable', optional('mut'))
       ,field('index', $.identifier_list)
       ,'in'
       ,field('data', $.tuple_list)
@@ -164,7 +178,7 @@ module.exports = grammar({
     )
     ,while_statement: $ => seq(
       'while'
-      ,field('condition', $.expression)
+      ,field('condition', $._expression)
       ,field('code', $.scope_statement)
     )
     ,match_statement: $ => seq(
@@ -175,97 +189,112 @@ module.exports = grammar({
       ,'}'
     )
     ,match_list: $ => repeat1(seq(
-      choice(
-        seq('==', $.expression)
+      field('condition', choice(
+        seq('==', $._expression)
         ,seq('in', $.expression_list)
-        ,$.expression
+        ,$._expression
         ,'else'
-      )
-      ,$.scope_statement
+      ))
+      ,field('code', $.scope_statement)
     ))
-    ,test_statement: $ => prec.right(seq('test', $.expression, $.scope_statement))
-    ,puts_statement: $ => prec.right(seq('puts', optional($.expression_list)))
-    ,print_statement: $ => prec.right(seq('print', optional($.expression_list)))
-    ,step_statement: $ => prec.right(seq('step', optional($.expression)))
-    ,wait_statement: $ => prec.right(seq('waitfor', $.expression))
-    ,lec_statement: $ => prec.left(seq('lec', $.expression_list))
-    ,poke_statement: $ => prec.left(seq('poke', $.expression_list))
-    ,peek_statement: $ => prec.left(seq('peek', $.expression_list))
+    ,expression_statement: $ => prec.right(seq(
+      choice(
+        $.identifier
+        ,$.constant
+        ,$.selection
+        ,$.type_specification
+        ,$.type_cast
+        ,$.function_call
+        ,$.function_definition
+        ,$.tuple
+        ,$.unary_expression
+        ,$.optional_expression
+        ,$.binary_expression
+      )
+      ,$._semicolon
+    ))
+    ,test_statement: $ => prec.right(seq(
+      'test'
+      ,field('name', $.string_literal)
+      ,field('condition', optseq('when', $._expression))
+      ,field('code', $.scope_statement)
+    ))
+    ,restrict_statement: $ => prec.right(seq(
+      'restrict'
+      ,field('name', $.string_literal)
+      ,field('condition', $._expression)
+      ,field('code', $.scope_statement)
+    ))
     ,expression_list: $ => prec.left(seq(
-      $.expression, repseq(',', $.expression)
+      $._expression, repseq(',', $._expression)
     ))
 
     // Function Call
-    // ,simple_function_call: $ => prec.left(seq(
-    //   $.identifier
-    //   ,$.expression_list
-    // ))
+    ,simple_function_call: $ => prec.left(seq(
+      field('name', $.identifier)
+      ,field('input', $.expression_list)
+    ))
 
     // Tuple
     ,tuple: $ => prec.left(seq(
-      '('
-      ,optional($.tuple_list)
-      ,')'
-      ,optional($.type_cast)
+      '(', optional($.tuple_list), ')', optional($.type_cast)
     ))
     ,tuple_list: $ => prec.left('tuple_list', seq(
       repeat(',')
-      ,$.tuple_item
-      ,repeat(seq(repeat1(','), $.tuple_item))
+      ,$._tuple_item
+      ,repeat(seq(repeat1(','), $._tuple_item))
       ,repeat(',')
     ))
-    ,tuple_item: $ => prec.left(choice(
-      $.expression
+    ,_tuple_item: $ => prec.left(choice(
+      $._expression
       ,$.assignment_or_declaration
     ))
     
     // Assignment/Declaration
-    ,assignment_or_declaration: $ => choice(
-      $.assignment
-      ,$.declaration
-    )
+    ,assignment_or_declaration: $ => choice($.assignment, $.declaration)
     ,assignment: $ => prec.right(seq(
       choice(
-        seq(
-          field('debug', optional('debug'))
-          ,field('scope', optional('pub'))
-          ,field('qualifier', optional($.type_qualifier))
-        )
+        field('debug', optional('debug'))
         ,field('overflow', optional(choice('wrap', 'saturate')))
       )
       ,field('lvalue', $.expression_list)
       ,field('operator', $.assignment_operator)
       ,field('delay', optional(choice($.cycle_select, '#')))
       ,field('rvalue', choice(
-        $.expression
-        ,$.introspection
-        ,$.library_import
+        $._expression
+        ,$.simple_function_call
       ))
     ))
     ,declaration: $ => seq(
       choice(
-        seq(field('scope', optional('pub')) ,field('qualifier', $.type_qualifier))
+        seq(field('scope', optional('pub')), field('qualifier', $.type_qualifier))
         ,seq(field('scope', 'pub'))
       )
       ,field('lvalue', $.expression_list)
+      ,optseq(
+        field('operator', $.assignment_operator)
+        ,field('delay', optional(choice($.cycle_select, '#')))
+        ,field('rvalue', choice(
+          $._expression
+          ,$.simple_function_call
+        ))
+      )
     )
     ,function_definition: $ => seq(
       field('func_type', choice('fun', 'proc'))
-      ,optseq('[', $.capture_list, ']')
-      ,optseq('<',  $.identifier_list, '>')
-      ,$.tuple
-      ,optseq('->', $.tuple)
-      ,optseq('where', $.expression)
-      ,$.scope_statement
+      ,field('capture', optseq('[', $.capture_list, ']'))
+      ,field('generic', optseq('<',  $.identifier_list, '>'))
+      ,field('input', seq('(', optional($.tuple_list), ')'))
+      ,field('output', optseq('->', '(', optional($.tuple_list), ')'))
+      ,field('condition', optseq('where', $._expression))
+      ,field('code', $.scope_statement)
     )
-    ,capture_list: $ => listseq1($.identifier, optseq('=', $.expression))
-    ,identifier_list: $ => listseq1($.identifier)
-    ,introspection: $ => prec.left(seq('punch', $.expression))
-    ,library_import: $ => prec.left(seq('import', $.expression))
+    ,capture_list: $ => listseq1($.identifier, optseq('=', $._expression))
+    ,identifier_list: $ => prec.left(listseq1($.identifier))
 
     // Expressions
-    ,expression: $ => prec.left(choice(
-      $.primary
+    ,_expression: $ => prec.left(choice(
+      $.identifier
       ,$.constant
       ,$.selection
       ,$.type_specification
@@ -280,23 +309,34 @@ module.exports = grammar({
       ,$.if_expression
       ,$.match_expression
       ,$.scope_expression
-      // ,seq('(', $.expression, ')')
     ))
     ,selection: $ => prec.right('selection', choice(
       $.member_selection
       ,$.bit_selection
       ,$.cycle_selection
     ))
-    ,member_selection: $ => seq($.expression, $.member_select)
-    ,bit_selection: $ => seq($.expression, $.bit_select)
-    ,cycle_selection: $ => seq($.expression, $.cycle_select)
-    ,type_specification: $ => seq($.expression, $.type_cast)
+    ,member_selection: $ => seq(
+      field('argument', $._expression)
+      ,field('select', $.member_select)
+    )
+    ,bit_selection: $ => seq(
+      field('argument', $._expression)
+      ,field('select', $.bit_select)
+    )
+    ,cycle_selection: $ => seq(
+      field('argument', $._expression)
+      ,field('select', $.cycle_select)
+    )
+    ,type_specification: $ => seq(
+      field('argument', $._expression)
+      ,field('type', $.type_cast)
+    )
     ,unary_expression: $ => prec.left('unary', seq(
-      field('operator', choice('!', 'not', '~', '-', '...', 'no'))
-      ,field('argument', $.expression)
+      field('operator', choice('!', 'not', '~', '-', '...', 'no', 'unless', 'when'))
+      ,field('argument', $._expression)
     ))
     ,optional_expression: $ => prec.right(seq(
-      field('argument', $.expression)
+      field('argument', $._expression)
       ,field('operator', '?')
     ))
     ,binary_expression: $ => choice(
@@ -337,14 +377,14 @@ module.exports = grammar({
         ,['.', 'member']
       ].map(([operator, precedence]) =>
         prec.left(precedence, seq(
-          field('left', $.expression)
+          field('left', $._expression)
           ,field('operator', operator)
-          ,field('right', $.expression)
+          ,field('right', $._expression)
         ))
       )
     )
     ,function_call: $ => prec.right('function', seq(
-      field('name', $.primary)
+      field('name', $._expression)
       ,'('
       ,optional($.tuple_list)
       ,')'
@@ -353,21 +393,10 @@ module.exports = grammar({
     ,if_expression: $ => prec.right('expression', alias($.if_statement, $.if_expression))
     ,match_expression: $ => prec.right('expression', alias($.match_statement, $.match_expression))
     ,scope_expression: $ => prec.right('expression', alias($.scope_statement, $.scope_expression))
-
-    ,constant: $ => prec.left(seq(
-      choice(
-        $.number
-        ,$.bool_literal
-        ,$.string_literal
-      )
-    ))
-    ,primary: $ => prec.left(seq(
-      $.hierarchical_identifier
-    ))
-
+    
     // Operators
     ,assignment_operator: $ => token(choice(
-      '=', '+=', '-=', '*=', '/=', '|=', '&=', '^=', '<<=', '>>=', '++=' /*, 'or=', 'and=' */
+      '=', '+=', '-=', '*=', '/=', '|=', '&=', '^=', '<<=', '>>=', '++=', 'or=', 'and='
     ))
 
     // Selects
@@ -376,8 +405,8 @@ module.exports = grammar({
       ,optional(choice(
         $.tuple_list
         // TODO: clean this up
-        ,seq($.tuple_item, '..')
-        ,seq(choice('..=', '..<', '..+'), $.tuple_item)
+        ,seq($._tuple_item, '..')
+        ,seq(choice('..=', '..<', '..+'), $._tuple_item)
       ))
       ,']'
     )
@@ -394,13 +423,17 @@ module.exports = grammar({
     ,type_qualifier: $ => choice('var', 'mut', 'let', 'reg')
 
     // Types
-    ,type_cast: $ => prec.left(seq(':', $.type, optional('?')))
-    ,type: $ => choice(
+    ,type_cast: $ => prec.left(seq(
+      ':'
+      ,field('type', $._type)
+      ,field('optional', optional('?'))
+    ))
+    ,_type: $ => choice(
       $.primitive_type
       ,$.tuple_type
       ,$.array_type
       ,$.function_type
-      ,$.hierarchical_identifier
+      ,$.identifier
       ,$.constant
       // TODO: Support type from expression
     )
@@ -408,11 +441,11 @@ module.exports = grammar({
       '(', $.tuple_list, ')'
     ))
     ,array_type: $ => prec.right(repeat1($.select))
-    ,function_type: $ => prec.left(seq(
-      field('func_type', choice('fun', 'proc'))
-      ,optseq('<',  $.identifier_list, '>')
-      ,optseq('(', optional($.tuple_list), ')')
-      ,optseq('->', '(', optional($.tuple_list), ')')
+    ,function_type: $ => prec.right(seq(
+      field('type', choice('fun', 'proc'))
+      ,field('generic', optseq('<',  $.identifier_list, '>'))
+      ,field('input', optseq('(', optional($.tuple_list), ')'))
+      ,field('output', optseq('->', '(', optional($.tuple_list), ')'))
     ))
     ,primitive_type: $ => prec.left(choice(
       $.unsized_integer_type
@@ -432,11 +465,23 @@ module.exports = grammar({
     ,bounded_integer_type: $ => seq(
       $.unsized_integer_type
       ,'('
-      ,optseq('max', '=')
-      ,$.expression
-      ,','
-      ,optseq('min', '=')
-      ,$.expression
+      ,choice(
+        seq(
+          optseq('max', '=')
+          ,field('max_value', $._expression)
+          ,','
+          ,optseq('min', '=')
+          ,field('min_value', $._expression)
+        )
+        ,seq(
+          field('min_value', $._expression)
+          ,'..'
+        )
+        ,seq(
+          '..'
+          ,field('max_value', $._expression)
+        )
+      )
       ,')'
     )
     ,sized_integer_type: $ => token(/[siu]\d+/)
@@ -445,10 +490,6 @@ module.exports = grammar({
     ,boolean_type: $ => token('boolean')
 
     // Identifiers
-    ,hierarchical_identifier: $ => prec.left(seq(
-      $.identifier
-      ,repseq('.', $.identifier)
-    ))
     ,identifier: $ => token(
       choice(
         // \p{L}  : Letter
@@ -463,6 +504,15 @@ module.exports = grammar({
         )
       )
     )
+
+    // Constants
+    ,constant: $ => prec.left(seq(
+      choice(
+        $.number
+        ,$.bool_literal
+        ,$.string_literal
+      )
+    ))
 
     // Numbers
     ,number: $ => choice(
@@ -479,6 +529,8 @@ module.exports = grammar({
     ,decimal_number: $ => token(/0(s|S)?(d|D)?[0-9][0-9_]*/)
     ,octal_number:   $ => token(/0(s|S)?(o|O)[0-7][0-7_]*/)
     ,binary_number:  $ => token(/0(s|S)?(b|B)[0-1\?][0-1_\?]*/)
+    
+    // Booleans
     ,bool_literal:   $ => token(choice('true', 'false'))
 
     // Strings
@@ -492,12 +544,11 @@ module.exports = grammar({
     )
 
     // Special
-    ,_space:   $ => token(/\s/)
-    // ,_space: $ => token(/[\s\f\uFEFF\u2060\u200B]|\\\r?\n/)
+    ,_space:   $ => token(/[\s\p{Zs}\uFEFF\u2060\u200B]/)
     ,_comment: $ => token(choice(
       /\/\/.*/
       ,seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/')
     ))
-    ,_newline: $ => token(/[;\n\r]/)
+    ,_semicolon: $ => choice($._automatic_semicolon, ';')
   }
 });
---
+++ src/scanner.c
@@ -0,0 +1,97 @@
+/*
+ * Reference: https://github.com/tree-sitter/tree-sitter-javascript/blob/4213a6c331d068b67de17e2dac917a90968cd33c/src/scanner.c
+ */
+
+#include <tree_sitter/parser.h>
+#include <wctype.h>
+
+enum TokenType {
+  AUTOMATIC_SEMICOLON
+};
+
+void *tree_sitter_pyrope_external_scanner_create() { return NULL; }
+void tree_sitter_pyrope_external_scanner_destroy(void *p) {}
+void tree_sitter_pyrope_external_scanner_reset(void *p) {}
+unsigned tree_sitter_pyrope_external_scanner_serialize(void *p, char *buffer) { return 0; }
+void tree_sitter_pyrope_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
+
+static void advance(TSLexer *lexer) { lexer->advance(lexer, false); }
+
+static bool scan_whitespace_and_comments(TSLexer *lexer) {
+  for (;;) {
+    while (iswspace(lexer->lookahead)) {
+      advance(lexer);
+    }
+
+    if (lexer->lookahead == '/') {
+      advance(lexer);
+
+      if (lexer->lookahead == '/') {
+        advance(lexer);
+        while (lexer->lookahead != 0 && lexer->lookahead != '\n') {
+          advance(lexer);
+        }
+      } else if (lexer->lookahead == '*') {
+        advance(lexer);
+        while (lexer->lookahead != 0) {
+          if (lexer->lookahead == '*') {
+            advance(lexer);
+            if (lexer->lookahead == '/') {
+              advance(lexer);
+              break;
+            }
+          } else {
+            advance(lexer);
+          }
+        }
+      } else {
+        return false;
+      }
+    } else {
+      return true;
+    }
+  }
+}
+
+bool tree_sitter_pyrope_external_scanner_scan(void *payload, TSLexer *lexer,
+                                                  const bool *valid_symbols) {
+  lexer->result_symbol = AUTOMATIC_SEMICOLON;
+  lexer->mark_end(lexer);
+
+  for (;;) {
+    if (lexer->lookahead == 0) return true;
+    if (lexer->lookahead == '}') return true;
+    if (lexer->is_at_included_range_start(lexer)) return true;
+    if (!iswspace(lexer->lookahead)) return false;
+    if (lexer->lookahead == '\n') break;
+    advance(lexer);
+  }
+
+  advance(lexer);
+
+  if (!scan_whitespace_and_comments(lexer)) return false;
+
+  switch (lexer->lookahead) {
+    case ',':
+    case '=':
+    case '.':
+    case '>':
+    case '<':
+    case '&':
+    case '^':
+    case '|':
+    case '*':
+    case '/':
+    case '+':
+    case '-':
+      return false;
+
+    // Don't insert a semicolon before `!=`, but do insert one before a unary `!`.
+    case '!':
+      advance(lexer);
+      return lexer->lookahead != '=';
+  }
+
+  return true;
+  
+}
