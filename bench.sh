860770a91ea1e1b8fd5434a07b7eff5baa024cc0
Jose Renau <renau@ucsc.edu>
2022-10-19 15:10:44 -0700

    update in docs.prp

--- bench.sh
+++ bench.sh
@@ -2,7 +2,7 @@
 
 mkdir -p benchtest
 rm -f benchtest/large1.prp
-for a in snippets/*prp
+for a in snippets/docs.prp
 do
   echo "// ${a}" >>benchtest/large1.prp
   cat $a         >>benchtest/large1.prp
@@ -12,7 +12,7 @@ cat benchtest/large1.prp benchtest/large1.prp benchtest/large1.prp benchtest/lar
 cat benchtest/large2.prp benchtest/large2.prp benchtest/large2.prp benchtest/large2.prp >benchtest/large3.prp
 cat benchtest/large3.prp benchtest/large3.prp benchtest/large3.prp benchtest/large3.prp >benchtest/large4.prp
 cat benchtest/large4.prp benchtest/large4.prp benchtest/large4.prp benchtest/large3.prp >benchtest/large5.prp
-cat benchtest/large5.prp benchtest/large5.prp benchtest/large5.prp benchtest/large5.prp >benchtest/large6.prp
+#cat benchtest/large5.prp benchtest/large5.prp benchtest/large5.prp benchtest/large5.prp >benchtest/large6.prp
 
 for a in benchtest/large*;
 do
