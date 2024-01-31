71e046c011368c43c6b8e6d18b85328fac93d75b
Russell Baxt <russellbaxt@gmail.com>
2024-01-30 23:24:19 -0800

    add tree traversal

--- prpfmt/prpfmt.c
+++ prpfmt/prpfmt.c
@@ -2,54 +2,58 @@
 #include <stdio.h>
 #include <tree_sitter/api.h>
 
-int file_to_string(char *fp, char *string);
+bool depth_first_traversal(TSNode *node);
 
 int main(int argc, char **argv) {
-   // Open prp file and assign to string
-   if (argc < 2) 
-      printf("Argument of type file path expected\n"), exit(1);
+  if (argc < 2)
+    printf("Argument of type file path expected\n"), exit(1);
 
-   char *buffer;
+  char *buffer;
 
-   FILE *fp = fopen(argv[1], "r");
-   if (!fp) perror(argv[1]), exit(EXIT_FAILURE);
+  FILE *fp = fopen(argv[1], "r"); 
+  if (!fp) perror(argv[1]), exit(EXIT_FAILURE);
 
-   fseek(fp, 0L, SEEK_END);
-   long lSize = ftell(fp);
-   rewind(fp);
+  fseek(fp, 0L, SEEK_END);
+  long l_size = ftell(fp);
+  rewind(fp);
 
-   buffer = malloc(lSize + 1);
-   if (!buffer) fclose(fp), fputs("memory allocation fails", stderr), exit(1);
+  buffer = malloc(l_size + 1);
+  if (!buffer) fclose(fp), fputs("Memory allocation fails", stderr), exit(1);
+  
+  if (1 != fread(buffer, l_size, 1, fp))
+    fclose(fp), free(buffer), fputs("Read fails", stderr), exit(1);
 
-   if (1 != fread(buffer, lSize, 1, fp))
-      fclose(fp), free(buffer), fputs("read fails", stderr), exit(1);
-   
-   fclose(fp);
+  fclose(fp);
 
-   printf("%s", buffer);
+  printf("%s", buffer);
+  
+  // Parsing
+  TSLanguage *tree_sitter_pyrope();
+  TSParser *parser = ts_parser_new();
+  ts_parser_set_language(parser, tree_sitter_pyrope());
 
-   // Parse string 
-   TSLanguage *tree_sitter_pyrope();
-   TSParser *parser = ts_parser_new();
+  TSTree *tree = ts_parser_parse_string(
+    parser, 
+    NULL, 
+    buffer, 
+    strlen(buffer)
+  );
 
-   ts_parser_set_language(parser, tree_sitter_pyrope());
+  TSNode root_node = ts_tree_root_node(tree);
+  
+  depth_first_traversal(&root_node);
 
-   TSTree *tree = ts_parser_parse_string(
-      parser,
-      NULL,
-      buffer,
-      strlen(buffer)
-   );
-
-   TSNode root_node = ts_tree_root_node(tree);
-
-   char *string = ts_node_string(root_node);
-
-   printf("%s\n", string);
-
-   ts_tree_delete(tree);
-   ts_parser_delete(parser);
-   free(buffer);
-   return 0;
+  ts_tree_delete(tree);
+  ts_parser_delete(parser);
+  free(buffer);
+  return 0;
 }
 
+bool depth_first_traversal(TSNode *node) {
+  printf("%s\n", ts_node_type(*node));
+  for (int child = 0; child < ts_node_child_count(*node); child++) {
+    TSNode child_node = ts_node_child(*node, child);
+    depth_first_traversal(&child_node);
+  }
+  return true;
+}
